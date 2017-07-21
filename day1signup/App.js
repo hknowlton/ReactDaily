97b74af607e16f93d57193d9f8b9ea3cf3e50d60
Heather Knowlton <heatherknowlton79@gmail.com>
2017-07-21 08:25:12 -0400

    working on sign up

---
+++ day1signup/App.js
@@ -0,0 +1,34 @@
+var App = React.createClass({
+    
+    getInitialState: function() {
+        return { mounted: false };
+    },
+    
+    componentDidMount: function() {
+        this.setState({ mounted: true });
+    },
+    
+    handleSubmit: function(e) {
+        this.setState({ mounted: false });
+        e.preventDefault();
+    },
+
+    render: function() {
+        var child;
+
+        if(this.state.mounted) {
+            child = (<Modal onSubmit={this.handleSubmit} />);
+        }
+        
+        return(
+            <div className="App">
+                <ReactCSSTransitionGroup 
+                    transitionName="example"
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={300}>
+                        {child}
+                </ReactCSSTransitionGroup>
+            </div>
+        );
+    }
+});
