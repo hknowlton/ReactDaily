2ae8bfb4f706006ce324ee406b0d4c8bc6c8fb2f
Heather Knowlton <heatherknowlton79@gmail.com>
2017-07-26 10:44:11 -0400

    all I do is route route route

--- rr4/src/App.js
+++ rr4/src/App.js
@@ -1,26 +1,40 @@
-import React  from 'react';
+import React from 'react';
 import {
-  BrowserRouter as Router, 
+  BrowserRouter as Router,
   Route,
-  Link
+  NavLink
 } from 'react-router-dom';
 
 const Home = (props) => {
-console.log(props);
-return <h1>What's up, I am the home component</h1>
+  return <h1>What's up, I am the home component</h1>
 }
 
-const App = ()=> ( 
+const Contact = (props) => {
+  return <h1>What's up, I am the Contact component. This is page number {1+2}
+  </h1>
+}
+//difference between Link and NavLink is that NavLink allows you to have an active state
+const NavLinks = () => (
+  <nav>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to ={{pathname: '/about'}}> About </NavLink>
+    <NavLink to ={{pathname: '/contact'}}> Contact </NavLink>
+  </nav>
+)
+
+const App = () => (
   <Router>
     <div>
-      <Route exact path="/" component={Home}/>
+      <NavLinks />
+      <Route exact path="/" component={Home} />
       {/* <Route path="/about" render={()=> <h1>Yoo from About</h1>}/> */}
-      <Route 
-        path="/about" 
-        children={({match })=> match &&<h1>Yoo from About</h1>}
+      <Route
+        path="/about"
+        children={({ match }) => match && <h1>Yoo from About</h1>}
       />
-      <Route path="/contact" component={contact}/>
+      <Route path="/contact" component={Contact} />
     </div>
   </Router>
 );
+
 export default App;
