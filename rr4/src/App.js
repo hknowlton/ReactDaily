371e58d350ef75c1a9e2350cf3202600b6f862fd
Heather Knowlton <hknowlton@it-mbp-hknowlton.corp.primedia.com>
2017-07-26 10:13:17 -0400

    egghead react router course

---
+++ rr4/src/App.js
@@ -0,0 +1,24 @@
+import React  from 'react';
+import {
+  BrowserRouter as Router, 
+  Route,
+  Link
+} from 'react-router-dom';
+
+const Home = (props) => {
+console.log(props);
+return <h1>What's up, I am the home component</h1>
+}
+
+const App = ()=> ( 
+  <Router>
+    <div>
+      <Route exact path="/" component={Home}/>
+      {/* <Route path="/about" render={()=> <h1>Yoo from About</h1>}/> */}
+      <Route 
+        path="/about" 
+        children={({match })=> match &&<h1>Yoo from About</h1>}/>
+    </div>
+  </Router>
+);
+export default App;
